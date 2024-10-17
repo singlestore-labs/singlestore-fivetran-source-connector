2690fed1924bece503f350466df82807ae482537
AdalbertMemSQL <amakarovych-ua@singlestore.com>
2024-10-17 15:55:46 +0300

    Added GitHub action for testing (#1)
    * Added GitHub action for testing

--- src/test/java/com/singlestore/fivetran/connector/IntegrationTestBase.java
+++ src/test/java/com/singlestore/fivetran/connector/IntegrationTestBase.java
@@ -1,7 +1,10 @@
 package com.singlestore.fivetran.connector;
 
 import com.google.common.collect.ImmutableMap;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.Statement;
+import java.util.Properties;
 import org.junit.jupiter.api.BeforeAll;
 
 public class IntegrationTestBase {
@@ -9,7 +12,7 @@ public class IntegrationTestBase {
   static String host = "127.0.0.1";
   static String port = "3306";
   static String user = "root";
-  static String password = "1";
+  static String password = System.getenv("ROOT_PASSWORD");
   static String database = "db";
 
   static SingleStoreConfiguration getConfig(String table) {
@@ -19,10 +22,14 @@ public class IntegrationTestBase {
 
   @BeforeAll
   public static void init() throws Exception {
-    SingleStoreConfiguration conf = getConfig("init");
-    SingleStoreConnection conn = new SingleStoreConnection(conf);
-    try (Statement stmt = conn.getConnection().createStatement()) {
+    String url = String.format("jdbc:singlestore://%s:%s", host, port);
+    Properties connectionProps = new Properties();
+    connectionProps.put("user", user);
+    connectionProps.put("password", password);
+    try (Connection conn = DriverManager.getConnection(url, connectionProps);
+        Statement stmt = conn.createStatement()) {
       stmt.execute("SET GLOBAL enable_observe_queries=1");
+      stmt.execute(String.format("CREATE DATABASE IF NOT EXISTS %s", database));
     }
   }
 }
